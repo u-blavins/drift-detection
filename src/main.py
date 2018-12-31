8aa6bb3b201b43088e2d5551eaeec61a1e931e67
u-blavins <usama.blavins1@gmail.com>
2018-12-31 20:52:11 +0000

    Updated main.py to include the AWS_Client

--- src/main.py
+++ src/main.py
@@ -1,13 +1,47 @@
-import boto3
 import json
+from utils.credentials import AWS_Client
 
 
-def main():
-    client = boto3.client('s3')
-    response = client.list_buckets()
-    print json.dumps(response, indent=4, default=str)
+def get_details():
+    """ Go into
+
+    :return:
+    """
+
+
+def get_stacks(client):
+    """ Make a call to AWS to retrieve CloudFormation
+    stacks
+
+    Args:
+        (obj)client: boto3 client
+    Returns:
+        (list)stacks: a list of CF stacks
+    """
+    stacks = []
+    paginator = client.get_paginator('describe_stacks')
+    response = paginator.paginate()
+    for page in response:
+        for stack in page['Stacks']:
+            stacks.append(stack)
+    return stacks
+
+
+# def detect_drift(stacks):
+#     return 0
+
+
+def main(aws_client):
+    """ Main function
+
+    Args:
+        (obj)aws_client: boto3 client
+    """
+    # stacks = get_stacks(aws_client)
+    # print json.dumps(stacks, indent=4, default=str)
     return 0
 
 
 if __name__ == '__main__':
-    main()
+    client = AWS_Client('account_number').client
+    main(client)
